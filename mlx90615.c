565e1516cba3fec5dd2b76a0ff570b96598fd654
shpi <lh@shpi.de>
2020-10-17 11:04:18 +0200

    added software buffer

--- mlx90615.c
+++ mlx90615.c
@@ -8,7 +8,7 @@
  *        Copyright (c) 2014 Peter Meerwald <pmeerw@pmeerw.net>,
  *        Copyright (c) 2015 Essensium NV
  *        Copyright (c) 2015 Melexis
- *        
+ *
  * Copyright (c) 2020 SHPI GmbH
  *
  *
@@ -31,7 +31,10 @@
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
-
+#include <linux/iio/buffer.h>
+#include <linux/iio/kfifo_buf.h>
+#include <linux/kthread.h>
+#include <linux/sched/task.h>
 
 #define MLX90615_OP_RAM           0x20
 #define MLX90615_OP_EEPROM        0x10
@@ -50,13 +53,13 @@ IIR 3 Bits
     data = data >> 12;
 
 000	0	forbidden
-001	1	100%         0.10s      1.0
+001	1	100%         0.10s     1.0
 010	2	50%          0.86s     0.5
 011	3	33.336%      1.4s      0.33
 100	4	25%          2.00s     0.25
-101	5	20%          3.00s      0.20
+101	5	20%          3.00s     0.20
 110	6	16.666%      3.3s      0.16
-111	7	14.286%      4.5s       0.14
+111	7	14.286%      4.5s      0.14
 
 cuts off spikes 100% means no cut off 
 
@@ -85,11 +88,16 @@ static const int mlx90615_iir_values[] = {0, 100, 50, 33, 25, 20, 16,  14};     
 
 static IIO_CONST_ATTR(in_temp_object_filter_low_pass_3db_frequency_available,
 		       "1.0 0.5 0.33 0.25 0.2 0.16 0.14");
-           
+
 struct mlx90615_data {
 	struct i2c_client *client;
 	struct mutex lock; /* for EEPROM access only */
 	unsigned long ready_timestamp; /* in jiffies */
+        struct task_struct *task;
+        struct {
+		u16 chan[2];
+		u64 ts __aligned(8);
+	} scan;
 };
 
 
@@ -200,7 +208,7 @@ static int mlx90615_read_raw(struct iio_dev *indio_dev,
 			cmd = MLX90615_TA;
 			break;
 		case IIO_MOD_TEMP_OBJECT:
-      cmd = MLX90615_TOBJ;
+                        cmd = MLX90615_TOBJ;
 			break;
 		default:
 			return -EINVAL;
@@ -276,12 +284,11 @@ static int mlx90615_write_raw(struct iio_dev *indio_dev,
 		val = val * MLX90615_CONST_RAW_EMISSIVITY_MAX +
 			val2 / MLX90615_CONST_EMISSIVITY_RESOLUTION;
 
-
 		mutex_lock(&data->lock);
 		ret = mlx90615_write_word(data->client, MLX90615_EMISSIVITY,
 					  val);
 		mutex_unlock(&data->lock);
-		
+
 
 		return ret;
 	case IIO_CHAN_INFO_LOW_PASS_FILTER_3DB_FREQUENCY: /* IIR Filter setting */
@@ -294,13 +301,149 @@ static int mlx90615_write_raw(struct iio_dev *indio_dev,
 					  val * 100 + val2 / 10000);
     /* write IIR */
 		mutex_unlock(&data->lock);
-		
+
 
 		return ret;
 	default:
 		return -EINVAL;
 	}
 }
+
+
+
+
+static int mlx90615_work_buffer(struct iio_dev *indio_dev)
+{
+	struct mlx90615_data *data = iio_priv(indio_dev);
+	int ret;
+	s64 time;
+
+
+	time = iio_get_time_ns(indio_dev);
+
+	/*
+	 * Single register reads: bulk_read will not work with ina226/219
+	 * as there is no auto-increment of the register pointer.
+	 */
+
+        if (test_bit(0, indio_dev->active_scan_mask))
+  	{
+
+		ret = i2c_smbus_read_word_data(data->client, MLX90615_TA);
+		if (ret < 0)
+			return ret;
+                if (ret & 0x8000)
+                        return -EIO;
+		data->scan.chan[0] = (unsigned int)ret;
+	}
+
+        if (test_bit(1, indio_dev->active_scan_mask)) 
+        {
+                ret = i2c_smbus_read_word_data(data->client, MLX90615_TOBJ);
+                if (ret < 0)
+                        return ret;
+                if (ret & 0x8000)
+                        return -EIO;
+                data->scan.chan[1] = (unsigned int)ret;
+        }
+
+	iio_push_to_buffers_with_timestamp(indio_dev, &data->scan, time);
+
+	return 0;
+};
+
+
+static int mlx90615_capture_thread(void *data)
+{
+	struct iio_dev *indio_dev = data;
+	int sampling_ms = 100;
+	int ret;
+	//struct timespec64 next, now, delta;
+	//s64 delay_us;
+
+
+	//ktime_get_ts64(&next);
+
+	do {
+
+		ret = mlx90615_work_buffer(indio_dev);
+		if (ret < 0)
+			return ret;
+
+		//ktime_get_ts64(&now);
+
+		/*
+		 * Advance the timestamp for the next poll by one sampling
+		 * interval, and sleep for the remainder (next - now)
+		 * In case "next" has already passed, the interval is added
+		 * multiple times, i.e. samples are dropped.
+		 */
+		/* do {
+			timespec64_add_ns(&next, 1000 * sampling_us);
+			delta = timespec64_sub(next, now);
+			delay_us = div_s64(timespec64_to_ns(&delta), 1000);
+		} while (delay_us <= 0);
+
+		usleep_range(delay_us, (delay_us * 3) >> 1);
+                */
+
+                msleep(sampling_ms);
+
+	} while (!kthread_should_stop());
+
+	return 0;
+}
+
+
+
+
+
+
+static int mlx90615_buffer_enable(struct iio_dev *indio_dev)
+{
+	struct mlx90615_data *data = iio_priv(indio_dev);
+	unsigned int sampling_ms = 100;
+	struct task_struct *task;
+
+	dev_info(&indio_dev->dev, "Enabling buffer w/ scan_mask %02x, freq = %d\n",
+		(unsigned int)(*indio_dev->active_scan_mask),
+		1000 / sampling_ms);
+
+	task = kthread_create(mlx90615_capture_thread, (void *)indio_dev,
+			      "%s:%d-%ums", indio_dev->name, indio_dev->id,
+			      sampling_ms);
+	if (IS_ERR(task))
+		return PTR_ERR(task);
+
+	get_task_struct(task);
+	wake_up_process(task);
+	data->task = task;
+
+	return 0;
+}
+
+
+static int mlx90615_buffer_disable(struct iio_dev *indio_dev)
+{
+	struct mlx90615_data *data = iio_priv(indio_dev);
+ dev_info(&indio_dev->dev, "Disabling buffer for MLX90615\n");
+
+
+	if (data->task) {
+		kthread_stop(data->task);
+		put_task_struct(data->task);
+		data->task = NULL;
+	}
+
+	return 0;
+}
+
+
+static const struct iio_buffer_setup_ops mlx90615_setup_ops = {
+	.postenable = &mlx90615_buffer_enable,
+	.predisable = &mlx90615_buffer_disable,
+};
+
 
 static int mlx90615_write_raw_get_fmt(struct iio_dev *indio_dev,
 				     struct iio_chan_spec const *channel,
@@ -309,7 +452,7 @@ static int mlx90615_write_raw_get_fmt(struct iio_dev *indio_dev,
 	switch (mask) {
 	case IIO_CHAN_INFO_CALIBEMISSIVITY:
 		return IIO_VAL_INT_PLUS_NANO;
-  case IIO_CHAN_INFO_LOW_PASS_FILTER_3DB_FREQUENCY:
+        case IIO_CHAN_INFO_LOW_PASS_FILTER_3DB_FREQUENCY:
 		return IIO_VAL_INT_PLUS_MICRO;
 	default:
 		return -EINVAL;
@@ -320,22 +463,41 @@ static const struct iio_chan_spec mlx90615_channels[] = {
 	{
 		.type = IIO_TEMP,
 		.modified = 1,
+                .indexed = 1,
 		.channel2 = IIO_MOD_TEMP_AMBIENT,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_OFFSET) |
 		    BIT(IIO_CHAN_INFO_SCALE),
+                .scan_index = 0,
+                .scan_type = {
+                        .sign = 'u',
+                        .realbits = 16,
+                        .storagebits = 16,
+                        .shift = 0,
+                        .endianness = IIO_BE,
+                },
 	},
 	{
 		.type = IIO_TEMP,
 		.modified = 1,
+                .indexed = 1,
 		.channel2 = IIO_MOD_TEMP_OBJECT,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW) |
 		    BIT(IIO_CHAN_INFO_CALIBEMISSIVITY) |
 		    BIT(IIO_CHAN_INFO_LOW_PASS_FILTER_3DB_FREQUENCY),
 		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_OFFSET) |
 		    BIT(IIO_CHAN_INFO_SCALE),
-	},
+                .scan_index = 1,
+                .scan_type = {
+                        .sign = 'u',
+                        .realbits = 16,
+                        .storagebits = 16,
+                        .shift = 0,
+                        .endianness = IIO_BE,
+                },
 
+	},
+IIO_CHAN_SOFT_TIMESTAMP(2),
 };
 
 static const struct iio_info mlx90615_info = {
@@ -355,7 +517,7 @@ static int mlx90615_probe(struct i2c_client *client,
 {
 	struct iio_dev *indio_dev;
 	struct mlx90615_data *data;
-
+        struct iio_buffer *buffer;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_WORD_DATA))
 		return -ENODEV;
@@ -370,12 +532,20 @@ static int mlx90615_probe(struct i2c_client *client,
 
 	indio_dev->dev.parent = &client->dev;
 	indio_dev->name = id->name;
-	indio_dev->modes = INDIO_DIRECT_MODE;
+	//indio_dev->modes = INDIO_DIRECT_MODE;
+        indio_dev->modes = INDIO_DIRECT_MODE | INDIO_BUFFER_SOFTWARE;
 	indio_dev->info = &mlx90615_info;
 	indio_dev->channels = mlx90615_channels;
-	indio_dev->num_channels = 2;
+	indio_dev->num_channels = ARRAY_SIZE(mlx90615_channels);
 
-	
+        indio_dev->setup_ops = &mlx90615_setup_ops;
+
+
+        buffer = devm_iio_kfifo_allocate(&indio_dev->dev);
+	if (!buffer)
+		return -ENOMEM;
+
+	iio_device_attach_buffer(indio_dev, buffer);
 
 	return iio_device_register(indio_dev);
 }
